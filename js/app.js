1a05b29eb91c991b0b0a5d32b1a26f94082058d1
Alberto Pérez-Bermejo <albperezbermejo@gmail.com>
2016-01-30 03:21:50 +0100

    feat: add regions & move data to sprite object

--- js/app.js
+++ js/app.js
@@ -2,25 +2,32 @@ console.log('loaded');
 
 var stage,
     playerA = {
-        framerate: 30,
-        "images": ["assets/grant.png"],
-        "frames": {
-            "regX": 82,
-            "regY": 0,
-            "width": 165,
-            "height": 292,
-            "count": 64
-        },
-        // define two animations, run (loops, 1.5x speed) and jump (returns to run):
-        "animations": {
-            "run": [0, 25, "run", 1.5],
-            "jump": [26, 63, "run"]
+        sprite: {},
+        data: {
+            framerate: 30,
+            "images": ["assets/grant.png"],
+            "frames": {
+                "regX": 82,
+                "regY": 0,
+                "width": 165,
+                "height": 292,
+                "count": 64
+            },
+            // define two animations, run (loops, 1.5x speed) and jump (returns to run):
+            "animations": {
+                "run": [0, 25, "run", 1.5],
+                "jump": [26, 63, "run"]
+            }
         }
-    };
+    },
+    regionParts = 8,
+    regionWidth;
 
 
 function init() {
     stage = new createjs.Stage(document.getElementById("gameCanvas"));
+    regionWidth = stage.canvas.width / regionParts;
+
     createjs.Ticker.timingMode = createjs.Ticker.RAF;
     createjs.Ticker.addEventListener("tick", stage);
 
@@ -28,19 +35,22 @@ function init() {
     resize(stage);
 
     loadPlayer(playerA);
+    moveSprite(playerA.sprite, 0);
 }
 
-function loadPlayer(spriteData) {
-    var spriteSheet = new createjs.SpriteSheet(spriteData),
-        sprite = new createjs.Sprite(spriteSheet, "run");
+function loadPlayer(player) {
+    var spriteSheet = new createjs.SpriteSheet(player.data);
+
+    player.sprite = new createjs.Sprite(spriteSheet, "run");
 
     spriteSheet.on("complete", onLoadComplete);
     spriteSheet.on("error", onLoadError);
+    stage.addChild(player.sprite);
+}
 
-    sprite.x = stage.canvas.width / 2;
-    sprite.y = 22;
-
-    stage.addChild(sprite);
+function moveSprite(sprite, pos) {
+    sprite.x = regionWidth * (pos+0.5);
+    sprite.y = (stage.canvas.height - sprite.spriteSheet._frameHeight) / 2;
 }
 
 function onLoadError(event) {
