ccbd874ccca2a9bc1662beb24b3c31b378b5e3ad
Alberto Pérez-Bermejo <alberto.perez-bermejo.contractor@bbva.com>
2016-01-30 02:22:55 +0100

    feat: refactor to functions, overflow hidden & auto canvas fullscreen

--- js/app.js
+++ js/app.js
@@ -1,12 +1,7 @@
 console.log('loaded');
 
-var stage;
-
-function init() {
-
-    stage = new createjs.Stage(document.getElementById("gameCanvas"));
-
-    var spriteSheet = new createjs.SpriteSheet({
+var stage,
+    playerA = {
         framerate: 30,
         "images": ["assets/grant.png"],
         "frames": {
@@ -21,21 +16,44 @@ function init() {
             "run": [0, 25, "run", 1.5],
             "jump": [26, 63, "run"]
         }
-    });
+    };
 
-    spriteSheet.on("complete", function(event) {
-        console.log("Complete", event);
-    });
-    spriteSheet.on("error", function(event) {
-        console.log("Error", event);
-    });
 
-    var grant = new createjs.Sprite(spriteSheet, "run");
-    grant.x = stage.canvas.width / 2;
-    grant.y = 22;
-
-    // Add Grant to the stage, and add it as a listener to Ticker to get updates each frame.
-    stage.addChild(grant);
+function init() {
+    stage = new createjs.Stage(document.getElementById("gameCanvas"));
     createjs.Ticker.timingMode = createjs.Ticker.RAF;
     createjs.Ticker.addEventListener("tick", stage);
+
+    window.addEventListener('resize', resize, false);
+    resize(stage);
+
+    loadPlayer(playerA);
+}
+
+function loadPlayer(spriteData) {
+    var spriteSheet = new createjs.SpriteSheet(spriteData),
+        sprite = new createjs.Sprite(spriteSheet, "run");
+
+    spriteSheet.on("complete", onLoadComplete);
+    spriteSheet.on("error", onLoadError);
+
+    sprite.x = stage.canvas.width / 2;
+    sprite.y = 22;
+
+    stage.addChild(sprite);
+}
+
+function onLoadError(event) {
+    console.log("Error", event);
+}
+
+function onLoadComplete(event) {
+    console.log("Complete", event);
+}
+
+function resize(stage) {
+    stage.canvas.width = window.innerWidth;
+    stage.canvas.height = window.innerHeight;
+
+
 }
